2d125b728758d150213726374ec44b8c8509a0e3
Stephanee Hayden <stephaneehayden@gmail.com>
2022-12-18 11:44:05 +1000

    JS updates user city input triggers api call new div and heading added to results container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,75 @@
 // Global Variables
 var userFormEl = document.querySelector('#user-form');
-var recentButtonsEl = document.querySelector('#recent-buttons');
 var cityInputEl = document.querySelector('#city-name');
+
+var recentButtonsEl = document.querySelector('#recent-buttons');
+
 var resultsContainerEl = document.querySelector('#results-container');
 var resultsSearchTerm = document.querySelector('#results-search-term');
 
-var APIKey = "f57655fbf21b91dc4e4940b14a581a26"; //default API key
-var city = "Brisbane" //FIXME: update based on cityInputEl.value
+// USER INPUT RESULTS
+var cityName = "" //FIXME: update based on cityInputEl.value
 var limit = 5; //FIXME: how many results to return from the city name list
-//get City Lat and Long
-var lat = -27.4679; //Brisbane
-var lon = 153.0281; //Brisbane
 
-//OpenWeather Request URLS
-var requestCityURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=" + limit + "&appid=" + APIKey;
-var requestLocationURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
-
-//get weather api
-
-console.log(requestCityURL);
-
-var getCurrentWeather = function (name) {
-    
-    fetch(requestCityURL)
-    .then(function (response) {
-        return response.json();
-    })
-
-    .then(function (data) {
-        console.log(data);  
-    });
-    
-  };
-
-var currentWeatherSection = function(cityName) {
-    // get and use data from open weather current weather api end point
-    fetch(requestLocationURL)
-        // get response and turn it into objects
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            // get city's longitude and latitude
-            var cityLon = response.coord.lon;
-            var cityLat = response.coord.lat;
-
-            console.log(cityLon);
-            console.log(cityLat);
-            console.log(requestLocationURL);
-        })
-};
+//OpenWeather Request variables
+var APIKey = "f57655fbf21b91dc4e4940b14a581a26"; //default API key
 
 
+//User enters city name and submits to update search results to current city
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    //Update cityName based on user input
+    var cityName = cityInputEl.value;
+
+    console.log(cityName);//TODO: REMOVE
+    getWeather(cityName); 
+
+}
+// Fetch current weather based on user input
+var getWeather = function (cityName) {
+    var queryCityURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&limit=" + limit + "&appid=" + APIKey + "&units=metric";  
+    console.log(queryCityURL); //TODO: REMOVE
+    //FETCH API
+        fetch(queryCityURL)
+            .then(function (weatherResponse) {
+                if (weatherResponse.ok) {
+                    weatherResponse.json().then(function(weatherResults) {
+                    //results of weather should now appear in results-container
+                        //add new div to hold weather results
+                        var currentWeatherEl = document.createElement("div");
+                        //add new heading to hold city name
+                        var titleEl = document.createElement("h2");
+                        titleEl.classList = "subtitle";
+                        titleEl.textContent = cityName;
+
+                        
+                        // ('<h2>').text(cityName)
+
+                        resultsContainerEl.append(currentWeatherEl);
+                        currentWeatherEl.append(titleEl);
+
+                        // currentWeatherEl.append(resultsContainerEl);
+                        // currentCityEl.append(currentWeatherEl);
+                    })
+                }
+            })
+        };
 
 
-getCurrentWeather();
-currentWeatherSection();
+
+
+
+
+
+
+// getWeather ();
+
+//run getWeather function when submit button is clicked
+//TODO: button should not run if no input entered
+userFormEl.addEventListener('submit', formSubmitHandler);
+
+
+
 //user inputs a city name
 //user selects the submit button
 //users search appears in recent searches section
