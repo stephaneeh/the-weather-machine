395b99b317aaeeb16ee585459a1c5439f8de2a19
Stephanee Hayden <stephaneehayden@gmail.com>
2022-12-18 14:05:46 +1000

    current day weather working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
 // Global Variables
 var userFormEl = document.querySelector('#user-form');
 var cityInputEl = document.querySelector('#city-name');
-
 var recentButtonsEl = document.querySelector('#recent-buttons');
-
 var resultsContainerEl = document.querySelector('#results-container');
-var resultsSearchTerm = document.querySelector('#results-search-term');
+var searchResults = document.querySelector('#search-results');
 
+var today = dayjs().format("ddd DD MMM, YYYY");
 // USER INPUT RESULTS
 var cityName = "" //FIXME: update based on cityInputEl.value
-var limit = 5; //FIXME: how many results to return from the city name list
+var limit = 1; //FIXME: how many results to return from the city name list
+var units = "metric";
 
 //OpenWeather Request variables
 var APIKey = "f57655fbf21b91dc4e4940b14a581a26"; //default API key
-
 
 //User enters city name and submits to update search results to current city
 var formSubmitHandler = function(event) {
@@ -25,38 +24,69 @@ var formSubmitHandler = function(event) {
     getWeather(cityName); 
 
 }
+
 // Fetch current weather based on user input
 var getWeather = function (cityName) {
-    var queryCityURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&limit=" + limit + "&appid=" + APIKey + "&units=metric";  
-    console.log(queryCityURL); //TODO: REMOVE
-    //FETCH API
-        fetch(queryCityURL)
-            .then(function (weatherResponse) {
-                if (weatherResponse.ok) {
-                    weatherResponse.json().then(function(weatherResults) {
-                    //results of weather should now appear in results-container
-                        //add new div to hold weather results
-                        var currentWeatherEl = document.createElement("div");
-                        //add new heading to hold city name
-                        var titleEl = document.createElement("h2");
-                        titleEl.classList = "subtitle";
-                        titleEl.textContent = cityName;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&limit=" + limit + "&appid=" + APIKey + "&units=" + units;  
+    console.log(queryURL); //TODO: REMOVE
 
+    //FETCH city query - userd for verification TODO: remove at the end
+    //might be required for 5 day forecast.... look into
+    fetch(queryURL)
+        .then(function (response) {
+        response.json().then(function (data) {
+        console.log(data);
+
+        var cityLat = data.coord.lat;
+        var cityLon = data.coord.lon;
+
+        console.log(cityLat + ", " + cityLon); //TODO: remove at the end
+
+        var queryFullURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + APIKey + "&units=" + units;  
+        console.log(queryFullURL); //TODO: remove at the end)
+    //fetch weather information based on user input
+            fetch(queryFullURL)
+                .then(function (weatherResponse) {
+                    if (weatherResponse.ok) {
+                        weatherResponse.json().then(function(weatherResults) {
+                            //update results search term with cityname and date
+                            var titleEl = document.createElement("h2");
+                            titleEl.classList = "subtitle";
+                            titleEl.textContent = cityName + " - " + today;
                         
-                        // ('<h2>').text(cityName)
+                            searchResults.append(titleEl);
 
-                        resultsContainerEl.append(currentWeatherEl);
-                        currentWeatherEl.append(titleEl);
+                            //add new div to hold weather results
+                            //results of weather should now appear in results-container
+                            var currentWeatherEl = document.createElement("ul");
+                            resultsContainerEl.append(currentWeatherEl);
 
-                        // currentWeatherEl.append(resultsContainerEl);
-                        // currentCityEl.append(currentWeatherEl);
-                    })
-                }
+                            var currentWeatherDetails = [
+                                "id: " + weatherResults.weather[0].id,
+                                // id: references the icon list + d and n for day and night
+                                "weather: " + weatherResults.weather[0].main,
+                                "temp: " +  weatherResults.main.temp + "C",
+                                "humidity: " + weatherResults.main.humidity + "%",
+                                "wind speed: " + weatherResults.wind.speed + "M/S",
+                                ];
+                            console.log(currentWeatherDetails);
+
+                            for (var i = 0; i < currentWeatherDetails.length; i++) {
+                                var currentWeatherList = document.createElement("li");
+                                currentWeatherList.textContent = currentWeatherDetails[i];
+                                            //append to ul
+                            currentWeatherEl.append(currentWeatherList);
+                            }
+                        })
+                    }
+                })
             })
-        };
+        });
+    };
 
-
-
+// var weatherData = function (cityData) {
+//     var qer
+// }
 
 
 
